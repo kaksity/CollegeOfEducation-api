185c72e30f01f1ee9a97787a0732a1121d6f698a
kaksity <daudapona2@gmail.com>
2022-07-17 13:36:48 +0100

    Bug Fix

--- app/Http/Resources/V1/Applicant/Nce/ApplicantDetailResource.php
+++ app/Http/Resources/V1/Applicant/Nce/ApplicantDetailResource.php
@@ -23,17 +23,17 @@ class ApplicantDetailResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'passport' => new PassportResource($this->NcePassport),
-            'personal_data' => new PersonalDataResource($this->NcePersonalData),
-            'contact_data' => new ContactDataResource($this->NceContactData),
-            'employment_data' => EmploymentDataResource::collection($this->NceEmploymentData),
-            'examination_data' => ExaminationDataResource::collection($this->NceExaminationData),
-            'educational_background_data' => EducationalBackgroundDataResource::collection($this->dipEducationalBackground),
-            'extra_curricular_activity_data' => ExtraCurricularActivityDataResource::collection($this->NceExtraCurricularActivityData),
-            'held_responsibility_data' => HeldResponsibilityDataResource::collection($this->NceHeldResponsibilityData),
-            // 'application_status' => new ApplicationStatusResource($this->user->NceApplicationStatus),
-            'course_data' =>  new CourseDataResource($this->NceCourseData),
-            'examination_center_data' => new ExaminationCenterDataResource($this->NceExaminationCenterData),
+            'passport' => new PassportResource($this->ncePassport),
+            'personal_data' => new PersonalDataResource($this->ncePersonalData),
+            'contact_data' => new ContactDataResource($this->nceContactData),
+            'employment_data' => EmploymentDataResource::collection($this->nceEmploymentData),
+            'examination_data' => ExaminationDataResource::collection($this->nceExaminationData),
+            'educational_background_data' => EducationalBackgroundDataResource::collection($this->nceEducationalBackground),
+            'extra_curricular_activity_data' => ExtraCurricularActivityDataResource::collection($this->nceExtraCurricularActivityData),
+            'held_responsibility_data' => HeldResponsibilityDataResource::collection($this->nceHeldResponsibilityData),
+            // 'application_status' => new ApplicationStatusResource($this->user->nceApplicationStatus),
+            'course_data' =>  new CourseDataResource($this->nceCourseData),
+            'examination_center_data' => new ExaminationCenterDataResource($this->nceExaminationCenterData),
             
         ];
     }
