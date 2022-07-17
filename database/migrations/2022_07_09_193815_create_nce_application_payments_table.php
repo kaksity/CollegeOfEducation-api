7ab065c16537a0e4cf5485e4f4ef9fe6bb67630f
kaksity <daudapona2@gmail.com>
2022-07-17 09:37:59 +0100

    Bug Fix

--- database/migrations/2022_07_09_193815_create_nce_application_payments_table.php
+++ database/migrations/2022_07_09_193815_create_nce_application_payments_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('application_payments');
+        Schema::dropIfExists('nce_application_payments');
     }
 };
